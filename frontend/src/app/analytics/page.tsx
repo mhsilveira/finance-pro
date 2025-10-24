7015a218ada9119dcb2feadcc380885dc8fa57c2
Matheus Silveira <blanchimaah@gmail.com>
2025-10-24 10:53:56 -0300

    initial commit with mocks

---
+++ frontend/src/app/analytics/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import ChartsContainer from '@/ui/charts/ChartsContainer';
+
+export default function AnalyticsPage() {
+  return <ChartsContainer />;
+}
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import Providers from './providers';
+import './globals.css';
+import Navbar from '@/ui/layout/navbar';
+
+export const metadata: Metadata = {
+  title: 'Finance Pro',
+  description: 'Personal finance Playground with React/Redux',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
